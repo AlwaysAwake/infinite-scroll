d28afc16e6f7600262c89982f104d582c23d737b
AlwaysAwake <tellmewhy07@gmail.com>
2020-12-10 15:06:42 +0900

    :lipstick: 기본 마크업 구조 추가

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from './Header.scss';
+
+const Header = () => {
+  return <div className={style.wrapper}>Infinite Scroll</div>;
+};
+
+export default Header;

d1a7eb7d42e2584657df47d0b517cf4155ffe001
AlwaysAwake <tellmewhy07@gmail.com>
2020-12-10 16:36:40 +0900

    :sparkles: Created 포맷팅 추가

--- src/components/ContentItem.js
+++ src/components/ContentItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {format} from 'date-fns';
 import style from './ContentItem.scss';
 
 const ContentItem = ({id, itemKey, uuid, created}) => {
@@ -7,7 +8,7 @@ const ContentItem = ({id, itemKey, uuid, created}) => {
       <h3>Id: {id}</h3>
       <h5>Key: {itemKey}</h5>
       <h5>Uuid: {uuid}</h5>
-      <h5>Created: {created}</h5>
+      <h5>Created: {format(new Date(created), 'yyyy/MM/dd HH:mm:ss')}</h5>
     </div>
   );
 };

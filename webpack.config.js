fffcbd2a458ab15d64a324c0379f194e2e1f486a
AlwaysAwake <tellmewhy07@gmail.com>
2020-12-10 11:14:29 +0900

    :wrench: JS 전처리를 위한 Babel 설정 추가 * https://babeljs.io/docs/en/babel-preset-env * https://babeljs.io/docs/en/babel-preset-react

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,15 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   devtool: 'eval',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
